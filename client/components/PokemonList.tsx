db2bfa8a09398c3ddd36f01749b66fb2b0f6edfc
student <student@pop-os.localdomain>
2023-11-14 10:11:54 +1300

    initial commit, pokemon list renderinggit add

--- client/components/PokemonList.tsx
+++ client/components/PokemonList.tsx
@@ -1,19 +1,37 @@
 import { Pokemon } from '../../models/pokemon.ts'
 import PokemonListItem from './PokemonListItem.tsx'
+import { useQuery } from '@tanstack/react-query'
+import { getAllPokemon } from '../apis/pokemon.ts'
+import LoadingSpinner from './LoadingSpinner.tsx'
 
 export default function PokemonList() {
   // TODO: fetch the list of pokemon from the server
-  const pokemon: Pokemon[] = [
-    { id: 1, name: 'Bulbasaur' },
-    {
-      id: 2,
-      name: 'Ivysaur',
-    },
-    {
-      id: 3,
-      name: 'Venusaur',
-    },
-  ]
+  const {
+    data: pokemon,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ['pokemon'],
+    queryFn: () => getAllPokemon(),
+  })
+  if (isError) {
+    return <p>Invalid pokemon</p>
+  }
+  if (!pokemon || isLoading) {
+    return <LoadingSpinner />
+  }
+
+  // const pokemon: Pokemon[] = [
+  // { id: 1, name: 'Bulbasaur' },
+  // {
+  //   id: 2,
+  //   name: 'Ivysaur',
+  // },
+  // {
+  //   id: 3,
+  //   name: 'Venusaur',
+  // },
+  // ]
 
   return (
     <div>
